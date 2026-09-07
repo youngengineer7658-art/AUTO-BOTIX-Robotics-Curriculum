766d530f57a7e2de1e203113047a647e9aae4829
youngengineer7658-art <youngengineer7658@gmail.com>
2026-09-07 19:12:08 +0530

    Update Practical_43.ino

--- Week-06/Day-22/Practical_43.ino
+++ Week-06/Day-22/Practical_43.ino
@@ -1,1 +1,62 @@
+#define M11 2
+#define M12 4
+#define MFN 6
+#define M21 7
+#define M22 8
 
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(M11,OUTPUT);
+  pinMode(M12,OUTPUT);
+  pinMode(M21,OUTPUT);
+  pinMode(M22,OUTPUT);
+  pinMode(MFN,OUTPUT);
+
+  stopMotor();
+
+  Serial.println("Bluetooth Motor Control");
+}
+
+void motorON()
+{
+  analogWrite(MFN,255);
+
+  digitalWrite(M11,HIGH);
+  digitalWrite(M12,LOW);
+
+  digitalWrite(M21,HIGH);
+  digitalWrite(M22,LOW);
+}
+
+void stopMotor()
+{
+  analogWrite(MFN,0);
+
+  digitalWrite(M11,LOW);
+  digitalWrite(M12,LOW);
+
+  digitalWrite(M21,LOW);
+  digitalWrite(M22,LOW);
+}
+
+void loop()
+{
+  if(Serial.available())
+  {
+    char cmd=Serial.read();
+
+    if(cmd=='1')
+    {
+      motorON();
+      Serial.println("Motor ON");
+    }
+
+    if(cmd=='0')
+    {
+      stopMotor();
+      Serial.println("Motor OFF");
+    }
+  }
+}
