2e84a9ce3a232aa8e08e10b5083c0786a0dee73f
youngengineer7658-art <youngengineer7658@gmail.com>
2026-09-07 17:16:09 +0530

    Update Practical_01.ino

--- Week-01/Day-01/Practical_01.ino
+++ Week-01/Day-01/Practical_01.ino
@@ -1,1 +1,29 @@
+#include <Adafruit_NeoPixel.h>
 
+#define LED_PIN 11
+#define NUMPIXELS 1
+
+Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+  pixels.setBrightness(80);
+}
+
+void loop() {
+
+  // Sab LEDs OFF
+  pixels.clear();
+
+  // Sirf pehli LED RED ON
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+  pixels.show();
+
+  delay(500);
+
+  // Sab LEDs OFF
+  pixels.clear();
+  pixels.show();
+
+  delay(500);
+}
