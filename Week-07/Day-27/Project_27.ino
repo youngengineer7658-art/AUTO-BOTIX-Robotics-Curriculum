509ab7c18db9320177a362195d77648a766eba7b
Young engineer <youngengineer7658@gmail.com>
2026-09-16 17:59:15 +0530

    Update Project_27.ino

--- Week-07/Day-27/Project_27.ino
+++ Week-07/Day-27/Project_27.ino
@@ -1,14 +1,14 @@
 /*
 ==========================================
-Project-27 : IR Remote Switch
-==========================================
-POWER  -> All OFF
-1      -> RGB RED
-2      -> Buzzer
-3      -> Relay Toggle
-4      -> Servo 0°
-5      -> Servo 90°
-6      -> Servo 180°
+Project-27 : IR Remote Switch (No Relay)
+Autobotix Robotics Board
+
+POWER  -> All OFF        (0x12)
+1      -> RGB RED        (0x0A)
+2      -> Buzzer         (0x1B)
+4      -> Servo 0°       (0x0C)
+5      -> Servo 90°      (0x0D)
+6      -> Servo 180°     (0x0E)
 ==========================================
 */
 
@@ -20,14 +20,11 @@ POWER  -> All OFF
 #define SERVO_PIN   10
 #define RGB_PIN     11
 #define BUZZER_PIN  12
-#define RELAY_PIN   A2
 
 #define NUMPIXELS 8
 Adafruit_NeoPixel rgb(NUMPIXELS, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
 Servo servo1;
-
-bool relayState = false;
 
 unsigned long lastCommandTime = 0;
 const unsigned long DEBOUNCE_DELAY = 400;
@@ -37,10 +34,7 @@ void setup()
   Serial.begin(9600);
 
   pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(RELAY_PIN, OUTPUT);
-
   digitalWrite(BUZZER_PIN, LOW);
-  digitalWrite(RELAY_PIN, HIGH);
 
   servo1.attach(SERVO_PIN);
   servo1.write(90);
@@ -52,7 +46,7 @@ void setup()
   IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
 
   Serial.println("=== Project-27 ===");
-  Serial.println("IR Remote Switch Ready");
+  Serial.println("IR Remote Switch Ready (No Relay)");
 }
 
 void loop()
@@ -73,12 +67,6 @@ void loop()
 
     uint8_t cmd = IrReceiver.decodedIRData.command;
 
-    if (cmd == 0x0)
-    {
-      IrReceiver.resume();
-      return;
-    }
-
     lastCommandTime = millis();
 
     Serial.print("Command : 0x");
@@ -86,49 +74,34 @@ void loop()
 
     switch (cmd)
     {
-      case 0x09:
+      case 0x12: // POWER -> All OFF
         allOff();
         Serial.println("All OFF");
         break;
 
-      case 0x0D:
-        setRGB(255,0,0);
+      case 0xA: // Button 1 -> RGB RED
+        setRGB(255, 0, 0);
         Serial.println("RED");
         break;
 
-      case 0x19:
+      case 0x1B: // Button 2 -> Beep Buzzer
         beepBuzzer();
         Serial.println("Buzzer");
         break;
 
-      case 0x1B:
-        relayState = !relayState;
-
-        if(relayState)
-        {
-          digitalWrite(RELAY_PIN,LOW);
-          Serial.println("Relay ON");
-        }
-        else
-        {
-          digitalWrite(RELAY_PIN,HIGH);
-          Serial.println("Relay OFF");
-        }
-        break;
-
-      case 0x01:
+      case 0xC: // Button 4 -> Servo 0°
         servo1.write(0);
-        Serial.println("Servo 0");
+        Serial.println("Servo 0°");
         break;
 
-      case 0x11:
+      case 0xD: // Button 5 -> Servo 90°
         servo1.write(90);
-        Serial.println("Servo 90");
+        Serial.println("Servo 90°");
         break;
 
-      case 0x15:
+      case 0xE: // Button 6 -> Servo 180°
         servo1.write(180);
-        Serial.println("Servo 180");
+        Serial.println("Servo 180°");
         break;
     }
 
@@ -138,9 +111,9 @@ void loop()
 
 void setRGB(byte r, byte g, byte b)
 {
-  for(int i=0;i<NUMPIXELS;i++)
+  for(int i = 0; i < NUMPIXELS; i++)
   {
-    rgb.setPixelColor(i,rgb.Color(r,g,b));
+    rgb.setPixelColor(i, rgb.Color(r, g, b));
   }
   rgb.show();
 }
@@ -157,11 +130,7 @@ void allOff()
   rgb.clear();
   rgb.show();
 
-  digitalWrite(RELAY_PIN,HIGH);
-
   digitalWrite(BUZZER_PIN, LOW);
 
   servo1.write(90);
-
-  relayState=false;
 }
