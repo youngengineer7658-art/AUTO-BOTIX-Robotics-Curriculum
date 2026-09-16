ca632a93bb6ff392647fe8c975d46000155d5040
Young engineer <youngengineer7658@gmail.com>
2026-09-16 17:51:27 +0530

    Update Practical_54.ino

--- Week-07/Day-27/Practical_54.ino
+++ Week-07/Day-27/Practical_54.ino
@@ -1,6 +1,6 @@
 /*
 ==================================================
-Practical-54 : IR → RGB LEDs
+Practical-54 : IR -> RGB LEDs
 Autobotix Robotics Board
 
 TSOP      -> D3
@@ -18,43 +18,42 @@ RGB LEDs  -> 8
 
 Adafruit_NeoPixel rgb(NUMPIXELS, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
-//--------------- IR Commands ----------------
+//--------------- Updated Custom IR Commands ----------------
 
-#define POWER   0x09
+#define POWER   0x12
 
-#define BTN1    0x0D
-#define BTN2    0x19
-#define BTN3    0x1B
-#define BTN4    0x01
-#define BTN5    0x11
-#define BTN6    0x15
-#define BTN7    0x17
-#define BTN8    0x06
-#define BTN9    0x12
-#define BTN0    0x16
+#define BTN1    0xA
+#define BTN2    0x1B
+#define BTN3    0x1F
+#define BTN4    0xC
+#define BTN5    0xD
+#define BTN6    0xE
+#define BTN7    0x0
+#define BTN8    0xF
+#define BTN9    0x19
+#define BTN0    0x7   // (Mapped from RPT key)
 
-#define OK      0x05
+#define OK      0x1   // (Mapped from PAUSE key)
 
-#define UP      0x0E
-#define DOWN    0x1A
-#define LEFT    0x0A
-#define RIGHT   0x1E
+#define UP      0x6   // (Mapped from VOL+ key)
+#define DOWN    0x5   // (Mapped from VOL- key)
+#define LEFT    0x2   // (Mapped from PREV key)
+#define RIGHT   0x3   // (Mapped from NEXT key)
 
 //--------------------------------------------
 
 int brightness = 80;
-
 uint8_t colorIndex = 0;
 
 uint32_t colors[] =
 {
-  rgb.Color(255,0,0),      //Red
-  rgb.Color(0,255,0),      //Green
-  rgb.Color(0,0,255),      //Blue
-  rgb.Color(255,255,0),    //Yellow
-  rgb.Color(0,255,255),    //Cyan
-  rgb.Color(255,0,255),    //Magenta
-  rgb.Color(255,255,255)   //White
+  rgb.Color(255,0,0),     // Red
+  rgb.Color(0,255,0),     // Green
+  rgb.Color(0,0,255),     // Blue
+  rgb.Color(255,255,0),   // Yellow
+  rgb.Color(0,255,255),   // Cyan
+  rgb.Color(255,0,255),   // Magenta
+  rgb.Color(255,255,255)  // White
 };
 
 //--------------------------------------------
@@ -63,9 +62,9 @@ void showColor(uint32_t c)
 {
   rgb.setBrightness(brightness);
 
-  for(int i=0;i<NUMPIXELS;i++)
+  for(int i=0; i<NUMPIXELS; i++)
   {
-    rgb.setPixelColor(i,c);
+    rgb.setPixelColor(i, c);
   }
 
   rgb.show();
@@ -85,12 +84,11 @@ void rainbow()
 {
   for(long firstPixelHue=0; firstPixelHue<65536; firstPixelHue+=256)
   {
-    for(int i=0;i<NUMPIXELS;i++)
+    for(int i=0; i<NUMPIXELS; i++)
     {
       int pixelHue = firstPixelHue + (i * 65536L / NUMPIXELS);
 
-      rgb.setPixelColor(i,
-      rgb.gamma32(rgb.ColorHSV(pixelHue)));
+      rgb.setPixelColor(i, rgb.gamma32(rgb.ColorHSV(pixelHue)));
     }
 
     rgb.setBrightness(brightness);
@@ -110,20 +108,18 @@ void rainbow()
 
 void policeFlash()
 {
-  for(int j=0;j<10;j++)
+  for(int j=0; j<10; j++)
   {
-    for(int i=0;i<NUMPIXELS;i++)
-      rgb.setPixelColor(i,rgb.Color(255,0,0));
+    for(int i=0; i<NUMPIXELS; i++)
+      rgb.setPixelColor(i, rgb.Color(255,0,0));
 
     rgb.show();
-
     delay(120);
 
-    for(int i=0;i<NUMPIXELS;i++)
-      rgb.setPixelColor(i,rgb.Color(0,0,255));
+    for(int i=0; i<NUMPIXELS; i++)
+      rgb.setPixelColor(i, rgb.Color(0,0,255));
 
     rgb.show();
-
     delay(120);
   }
 
@@ -141,7 +137,7 @@ void setup()
 
   IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
 
-  Serial.println("Autobotix Practical-54");
+  Serial.println("Autobotix Practical-54 Ready");
 }
 
 //--------------------------------------------
@@ -150,113 +146,99 @@ void loop()
 {
   if(IrReceiver.decode())
   {
-    uint8_t cmd = IrReceiver.decodedIRData.command;
-
-    Serial.print("Command : 0x");
-    Serial.println(cmd,HEX);
-
-    switch(cmd)
+    if (!(IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT))
     {
-      case POWER:
-      case BTN0:
-        rgbOff();
-      break;
+      uint8_t cmd = IrReceiver.decodedIRData.command;
 
-      case BTN1:
-        colorIndex=0;
-        showColor(colors[colorIndex]);
-      break;
+      Serial.print("Command : 0x");
+      Serial.println(cmd, HEX);
 
-      case BTN2:
-        colorIndex=1;
-        showColor(colors[colorIndex]);
-      break;
+      switch(cmd)
+      {
+        case POWER:
+        case BTN0:
+          rgbOff();
+        break;
 
-      case BTN3:
-        colorIndex=2;
-        showColor(colors[colorIndex]);
-      break;
+        case BTN1:
+          colorIndex = 0;
+          showColor(colors[colorIndex]);
+        break;
 
-      case BTN4:
-        colorIndex=3;
-        showColor(colors[colorIndex]);
-      break;
+        case BTN2:
+          colorIndex = 1;
+          showColor(colors[colorIndex]);
+        break;
 
-      case BTN5:
-        colorIndex=4;
-        showColor(colors[colorIndex]);
-      break;
+        case BTN3:
+          colorIndex = 2;
+          showColor(colors[colorIndex]);
+        break;
 
-      case BTN6:
-        colorIndex=5;
-        showColor(colors[colorIndex]);
-      break;
+        case BTN4:
+          colorIndex = 3;
+          showColor(colors[colorIndex]);
+        break;
 
-      case BTN7:
-        colorIndex=6;
-        showColor(colors[colorIndex]);
-      break;
+        case BTN5:
+          colorIndex = 4;
+          showColor(colors[colorIndex]);
+        break;
 
-      case BTN8:
-        rainbow();
-      break;
+        case BTN6:
+          colorIndex = 5;
+          showColor(colors[colorIndex]);
+        break;
 
-      case BTN9:
-        policeFlash();
-      break;
+        case BTN7:
+          colorIndex = 6;
+          showColor(colors[colorIndex]);
+        break;
 
-      case OK:
-        colorIndex++;
+        case BTN8:
+          rainbow();
+        break;
 
-        if(colorIndex>6)
-          colorIndex=0;
+        case BTN9:
+          policeFlash();
+        break;
 
-        showColor(colors[colorIndex]);
-      break;
+        case OK:
+          colorIndex++;
+          if(colorIndex > 6)
+            colorIndex = 0;
+          showColor(colors[colorIndex]);
+        break;
 
-      case UP:
+        case UP:
+          brightness += 20;
+          if(brightness > 255)
+            brightness = 255;
+          showColor(colors[colorIndex]);
+        break;
 
-        brightness +=20;
+        case DOWN:
+          brightness -= 20;
+          if(brightness < 10)
+            brightness = 10;
+          showColor(colors[colorIndex]);
+        break;
 
-        if(brightness>255)
-          brightness=255;
+        case RIGHT:
+          colorIndex++;
+          if(colorIndex > 6)
+            colorIndex = 0;
+          showColor(colors[colorIndex]);
+        break;
 
-        showColor(colors[colorIndex]);
-
-      break;
-
-      case DOWN:
-
-        brightness -=20;
-
-        if(brightness<10)
-          brightness=10;
-
-        showColor(colors[colorIndex]);
-
-      break;
-
-      case RIGHT:
-
-        colorIndex++;
-
-        if(colorIndex>6)
-          colorIndex=0;
-
-        showColor(colors[colorIndex]);
-
-      break;
-
-      case LEFT:
-
-        if(colorIndex==0)
-          colorIndex=6;
-        else
-          colorIndex--;
-
-        showColor(colors[colorIndex]);
-
-      break;
+        case LEFT:
+          if(colorIndex == 0)
+            colorIndex = 6;
+          else
+            colorIndex--;
+          showColor(colors[colorIndex]);
+        break;
+      }
     }
 
     IrReceiver.resume();
